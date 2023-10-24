206c58720c4b9de462a8cb30f08085f1bc6a8923
pedrodoze <pedroigordourado@yahoo.com.br>
2023-10-23 23:34:59 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Calculator from './Calculator';
+import './index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Calculator />
+  </Provider>,
+  document.getElementById('root')
+);
